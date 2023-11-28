9ba3c3581e3338fae9943c28c71ff7c052adac9d
SAIKmar-1729 <83260137+SAIKmar-1729@users.noreply.github.com>
2023-11-28 19:39:16 +0530

    done

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors"
 import cookieParser from "cookie-parser";
+import { registerUser } from "./controllers/user.controller.js";
 
 const app = express();
 
@@ -13,5 +14,14 @@ app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true, limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
 
 export {app};
--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@
 
 import connectDB from "./db/index.js";
 import dotenv from 'dotenv';
+import {app} from './app.js'
 
 dotenv.config({
-    path:"../env"
+    path: "../env"
 })
 
 connectDB().then(
-    () =>{
-        app.listen(process.env.PORT || 8000,() => {
-            console.log(` Server is Running at port : ${process.env.PORT}`);
+    () => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
         })
     }
-).catch(()=>{
+).catch((err) => {
     console.log("MONGODB Connection Failed !!! ", err);
 })
 
