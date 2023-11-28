dd361465f244ca75667327cb40aa2e3c8f7a063d
SAIKmar-1729 <83260137+SAIKmar-1729@users.noreply.github.com>
2023-11-28 23:50:20 +0530

    commit when user is registered

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors"
 import cookieParser from "cookie-parser";
-import { registerUser } from "./controllers/user.controller.js";
+
 
 const app = express();
 
